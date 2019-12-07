23eef3c91b16a0093a8aba3cc3572e1a60802795
DaHoraDev <51419399+DaHoraDev@users.noreply.github.com>
2019-12-07 05:15:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,63 +1,70 @@
 #include <iostream>
-#include <clocale>
 
-using namespace std;
+#define verde "\x1b[32m"
+#define vermelho "\x1b[31m"
+#define clean "\x1b[0m"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
-    char v[100];
-    int i = 0;
-    int j = 0;
+    bool teste=true;
+    int tam;
+    int i=0;
+    int j=0;
+    string cadeia;
 
-    cout<<"Expressão regular => 1(23)*(01)* "<<endl;
-    cout<<"Digite uma cadeia: "; cin>>v;
+    cout<<"Digite uma cadeia: "; cin>>cadeia;
 
-    for(int c = 0; c < 100; c++){
-        if(v[c] != ' '){
-            v[c] = '#';
+    tam = cadeia.size(); /*Verifica o tamanho da string*/
+
+    /*Testa se a primeira posição da string é igual a 1 (equivalente a q0 -> q1)*/
+    if(cadeia[0] == '1'){
+        cout<<verde<<cadeia[i]<<clean<<" ";
+    }else{
+        cout<<vermelho<<cadeia[i]<<clean<<" ";
+    }
+    i++;
+    if(tam > 2){
+        while(cadeia[i] !=  || teste){
+            j = i + 1;
+
+            if(cadeia[i] == '2'){
+                cout<<verde<<cadeia[i]<<clean<<" ";
+
+                if(cadeia[j] == '3'){
+                    cout<<verde<<cadeia[j]<<clean<<" ";
+                }else{
+                    cout<<vermelho<<cadeia[j]<<clean<<" ";
+                    teste = false;
+                }
+
+            }else if(cadeia[i] == '0'){
+                cout<<verde<<cadeia[i]<<clean<<" ";
+
+                if(cadeia[j] == '1'){
+                    cout<<verde<<cadeia[j]<<clean<<" ";
+                }else{
+                    cout<<vermelho<<cadeia[j]<<clean<<" ";
+                    teste=false;
+                }
+
+            }else{
+                cout<<vermelho<<cadeia[i]<<clean<<" ";
+                teste = false;
+            }
+
+            i+=2;
+
         }
     }
 
-    //q0 -> q1
-    if(v[i] == '1'){
-        cout<<v[i]<<" ->aceito"<<endl;
+    if(teste){
+        cout<<verde<<"\nCadeia aceita"<<endl;
     }else{
-        cout<<v[i]<<" ->recusado"<<endl;
-    }
-
-    while(v[i] != '#'){
-
-        i++;
-        j = i+1;
-
-        //caso q1 -> q2
-        do{
-
-            if (v[i] == '2'){
-                cout << v[i] << " ->aceito" << endl;
-
-                if (v[j] == '3') {
-                    cout << v[j] << " ->aceito" << endl;
-                } else {
-                    cout << v[j] << " ->recusado" << endl;
-                }
-            }
-
-        }while(v[i] == '2' || v[j] == '3');
-
-        //Caso q1 -> q3
-        do {
-
-            if (v[i] == '0') {
-                cout << v[i] << " ->aceito" << endl;
-                if (v[j] == '1') {
-                    cout << v[j] << " ->aceito" << endl;
-                } else {
-                    cout << v[j] << " ->recusado" << endl;
-                }
-            }
-
-        }while(v[i] == '0' || v[j] == '1');
+        cout<<vermelho<<"\nCadeia não aceita"<<endl;
     }
 
     return 0;
